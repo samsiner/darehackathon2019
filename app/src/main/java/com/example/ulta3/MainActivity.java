122a66e89780b2368fdb18bdb38fae0535fc1eb6
wrinkleintime <ssiner2@gmail.com>
2019-10-27 05:28:02 -0500

    Add API capability

--- app/src/main/java/com/example/ulta3/MainActivity.java
+++ app/src/main/java/com/example/ulta3/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.ulta3;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.support.annotation.NonNull;
 import android.support.constraint.Constraints;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
@@ -20,8 +24,16 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.ulta3.model.Product;
 import com.example.ulta3.BuildConfig;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.opencsv.CSVReader;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
@@ -45,6 +57,8 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     ViewModel vm;
+    private FusedLocationProviderClient fusedLocationClient;
+    LocationManager mLocationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,7 +76,9 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
+        getClosestStore();
         buildDatabase();
 
         ArrayList<Integer> al = (ArrayList)vm.getManProducts();
@@ -70,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
         for (int i : al){
             newmap.put(vm.getNameBySKU(i), "");
         }
+
     }
 
     private static final int SPEECH_REQUEST_CODE = 0;
@@ -124,6 +141,162 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e1) {
             e1.printStackTrace();
         }
+    }
+
+    private String destinations = "";
+
+    private void getClosestStore(){
+        StringBuilder buildURL = new StringBuilder();
+        buildURL.append("https://maps.googleapis.com/maps/api/distancematrix/json?origins=");
+
+        try {
+            Task locationResult = fusedLocationClient.getLastLocation();
+
+            locationResult.addOnCompleteListener(this, new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if (task.isSuccessful()) {
+                        Location loc = (Location)task.getResult();
+                        buildURL.append(loc.getLatitude()+","+loc.getLongitude());
+                        buildURL.append("&destinations=");
+                        buildURL.append(destinations + "&key=");
+                        Log.d("LAT", buildURL.toString());
+
+                    }
+                }
+            });
+
+
+        } catch(SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+
+
+//        try{
+            InputStream is = getResources().openRawResource(R.raw.store_details);
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(is, Charset.forName("UTF-8")));
+            String line;
+
+            try {
+                reader.readLine();
+                while ((line = reader.readLine()) != null) {
+                    String[] tokens = line.split(",");
+
+                    if (!tokens[4].equals("Chicago")) continue;
+
+                    int storeID;
+                    try {
+                        storeID = Integer.parseInt(tokens[0]);
+                    } catch (NumberFormatException e){
+                        continue;
+                    }
+
+                    String address = tokens[2];
+                    String[] addressArr = address.split(" ");
+
+                    for (int i=0;i<addressArr.length;i++){
+                        destinations += addressArr[i] + "+";
+                    }
+
+                    String city = tokens[4];
+                    String[] cityArr = city.split(" ");
+
+                    for (int i=0;i<cityArr.length;i++){
+                        destinations += cityArr[i] + "+";
+                    }
+
+                    String state = tokens[5];
+                    destinations += state + "|";
+                }
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
+            destinations = destinations.substring(0, destinations.length()-1);
+//            URL urlStations = new URL(buildURL.toString());
+//
+//            Thread thread = new Thread() {
+//                public void run() {
+//                    try {
+//                        Scanner scan = new Scanner(urlStations.openStream());
+//                        while (scan.hasNext()) sb.append(scan.nextLine());
+//                        scan.close();
+//                    } catch (IOException e){
+//                        e.printStackTrace();
+//                    }
+//                }
+//            };
+//            thread.start();
+//            try{
+//                thread.join();
+//            } catch (InterruptedException e){
+//                e.printStackTrace();
+//            }
+//
+//        } catch (IOException e) {
+//            sb.delete(0, sb.length());
+//            sb.append("NO INTERNET");
+//        }
+//        String JSONString = sb.toString();
+//
+//        Log.d("JSONString", JSONString);
+
+//        //Set internet connection status
+//        connectionStatusLD.postValue(!JSONString.equals("NO INTERNET"));
+//        if (JSONString.equals("NO INTERNET")) return;
+//
+//        ArrayList<String> currentAlerts = new ArrayList<>(); //For multiple alerts
+//        ArrayList<String> beforeStationsOut = new ArrayList<>(mStationDao.getAllAlertStationIDs());
+//
+//        try {
+//            JSONObject outer = new JSONObject(JSONString);
+//            JSONObject inner = outer.getJSONObject("CTAAlerts");
+//            JSONArray arrAlerts = inner.getJSONArray("Alert");
+//
+//            for (int i=0;i<arrAlerts.length();i++){
+//                JSONObject alert = (JSONObject) arrAlerts.get(i);
+//                String impact = alert.getString("Impact");
+//                if (!impact.equals("Elevator Status")) continue;
+//
+//                JSONArray service;
+//                try {
+//                    JSONObject impactedService = alert.getJSONObject("ImpactedService");
+//                    service = impactedService.getJSONArray("Service");
+//                } catch (JSONException e){
+//                    e.printStackTrace();
+//                    continue;
+//                }
+//
+//                for (int j=0;j<service.length();j++) {
+//                    JSONObject serviceInstance = (JSONObject) service.get(j);
+//                    if (serviceInstance.getString("ServiceType").equals("T")) {
+//                        String id = serviceInstance.getString("ServiceId");
+//                        String headline = alert.getString("Headline");
+//
+//                        if (headline.contains("Back in Service")) break;
+//
+//                        //End up with beforeStationsOut only containing alerts that no longer exist
+//                        beforeStationsOut.remove(id);
+//
+//                        //Looking for multiple alerts for the same station.
+//                        String shortDesc = alert.getString("ShortDescription");
+//                        if (currentAlerts.contains(id)) {
+//                            shortDesc += "\n\n";
+//                            shortDesc += mStationDao.getShortDescription(id);
+//                        } else {
+//                            currentAlerts.add(id);
+//                        }
+//
+//                        mStationDao.setAlert(id, shortDesc);
+//                        break;
+//                    }
+//                }
+//            }
+//        }
+//        catch (JSONException | NullPointerException e) {
+//            e.printStackTrace();
+//        }
     }
 
     @Override
