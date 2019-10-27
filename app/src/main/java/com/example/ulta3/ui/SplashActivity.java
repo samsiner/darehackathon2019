b49ee8e67c4dd631373609c9768b96c018a62471
Tyler Arndt <tsarndt16@gmail.com>
2019-10-26 20:22:10 -0500

    splash screen to 3 second timer

--- app/src/main/java/com/example/ulta3/ui/SplashActivity.java
+++ app/src/main/java/com/example/ulta3/ui/SplashActivity.java
@@ -1,19 +1,33 @@
 package com.example.ulta3.ui;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.ulta3.MainActivity;
 
 public class SplashActivity extends AppCompatActivity {
+    private Handler mWaitHandler = new Handler();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+
+        mWaitHandler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                 try {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (Exception ignored) {
+                    ignored.printStackTrace();
+                }
+            }
+        }, 3000);
     }
 }
